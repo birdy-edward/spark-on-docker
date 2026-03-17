60fbb043a49b6f67c4643a7c3fa66b636a971b24
birdy-edward <tdthuycd2001@gmail.com>
2026-03-18 00:57:47 +0700

    update

---
+++ setup-java-home.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For OpenJDK 17
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# Or for OpenJDK 11
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+PATH=$JAVA_HOME/bin:$PATH
+
